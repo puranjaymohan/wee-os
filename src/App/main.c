af9f79f9a5a54ec6454c1cef6396e7bc1ac8b700
Puranjay Mohan <puranjay12@gmail.com>
2021-06-15 17:48:51 +0530

    Kernel: Add weighted round robin algorithm
    Add support for the round robin algorithm to the scheduler and allow th
    user to choose the algorithm in Makefile. The API remains same and main
    application just has to provide parameters required to the algorithm.
    
    Signed-off-by: Puranjay Mohan <puranjay12@gmail.com>

--- src/App/main.c
+++ src/App/main.c
@@ -1,32 +1,42 @@
 #include <stdint.h>
 #include <weeOs.h>
 
-int t1 = 0;
+int c0 = 0;
+int c1 = 0;
+int c2 = 0;
 
 void task0(void)
 {
 	while(1) {
+		c0++;
 	}
 }
 
 void task1(void)
 {
 	while(1) {
+		c1++;
 	}
 }
 
 void task2(void)
 {
 	while(1) {
+		c2++;
 	}
 }
 
 int main()
 {
+#ifdef weighted_round_robin
+	wee_os_addthread(task0, 1);
+	wee_os_addthread(task1, 2);
+	wee_os_addthread(task2, 3);
+#elif defined round_robin
 	wee_os_addthread(task0);
 	wee_os_addthread(task1);
 	wee_os_addthread(task2);
-
+#endif
 	wee_os_launch(1);
 	while(1);
 }
