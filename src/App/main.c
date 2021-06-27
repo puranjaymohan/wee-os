0e16696e908d7fdf7ed355387922ceac86fd6cb9
Abhay Chirania <abhaychirania2411@gmail.com>
2021-06-27 12:10:11 +0545

    Kernel: Add thread syscall

--- src/App/main.c
+++ src/App/main.c
@@ -31,15 +31,12 @@ void task2(void)
 
 int main()
 {
-#ifdef weighted_round_robin
-	wee_os_addthread(task0, 1);
-	wee_os_addthread(task1, 2);
-	wee_os_addthread(task2, 3);
-#elif defined round_robin
-	wee_os_addthread(task0);
-	wee_os_addthread(task1);
-	wee_os_addthread(task2);
-#endif
-	wee_os_launch(100);
+	task_prop task0_data = {.task = task0 , .name="TASK0"};
+	task_prop task1_data = {.task = task1 , .name="TASK1"};
+	task_prop task2_data = {.task = task2 , .name="TASK2"};
+	wee_os_addthread(&task0_data);
+	wee_os_addthread(&task1_data);
+	wee_os_addthread(&task2_data);
+	wee_os_launch(10);
 	while(1);
 }
--- src/Kernel/Src/system_call.c
+++ src/Kernel/Src/system_call.c
@@ -4,26 +4,29 @@
 
 extern tcb *current_tcb;
 extern char active_tasks[MAX_TASKS];
-
-uint32_t c=0;
-
-void wee_os_system_call(uint32_t syscall_no __attribute__((unused)))
+uint8_t c = 0;
+void wee_os_system_call(uint32_t syscall_no __attribute__((unused)) , 
+			task_prop *task_property __attribute__((unused)))
 {
 	 __asm__ volatile("svc 0");
 }
 
+
 void __attribute__((naked)) SVCall_Handler(void)
 {	__disable_irq();
 	uint32_t syscall_no = *(uint32_t *)__get_MSP();
+	task_prop *task_property = (task_prop *)*((uint32_t *)__get_MSP() + 1);
 	__asm__ volatile ("push {LR}");
-	switch(syscall_no)
-	{
-		case 1: wee_os_syscall_yield();
-			break;
-		case 2: wee_os_syscall_kill();
-			break;
-		default: c++;
-			break;
+	switch(syscall_no) {
+	case 1: wee_os_syscall_yield();
+		break;
+	case 2: wee_os_syscall_kill();
+		break;
+	case 3:
+		c = wee_os_add_thread(task_property);
+		break;
+	default:
+		break;
 	}
 	__enable_irq();
 	__asm__ volatile ("pop {LR}");
@@ -42,23 +45,17 @@ void wee_os_syscall_yield(void)
 
 void wee_os_syscall_kill(void)
 {
-        if(current_tcb->next_tcb == current_tcb)
-	{
+        if(current_tcb->next_tcb == current_tcb){
 		__enable_irq();
 		return;
-	}
-
-	if(current_tcb->pid != 0)
-	{
+	} else if(current_tcb->pid != 0) {
 		(current_tcb-1)->next_tcb = current_tcb->next_tcb;
-	}
-	else{
+	} else {
 		tcb *temp_tcb = current_tcb;
 		while(temp_tcb->next_tcb != current_tcb)
 			temp_tcb++;
 		temp_tcb->next_tcb = current_tcb->next_tcb;
 	}
-
 	active_tasks[current_tcb->pid] = 0;
 	__enable_irq();
 	wee_os_syscall_yield();
