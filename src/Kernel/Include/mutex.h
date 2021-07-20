6f4a6190349f1de5253aa2653f6d749a3c6561ca
Puranjay Mohan <puranjay12@gmail.com>
2021-07-20 14:57:32 +0530

    Kernel: add support for mutex lock
    Add spinlock implementation using STREX and LDREX opcodes.
    This implementation doesn't yield the CPU while waiting.
    
    Signed-off-by: Puranjay Mohan <puranjay12@gmail.com>

---
+++ src/Kernel/Include/mutex.h
@@ -0,0 +1,11 @@
+#ifndef __MUTEX_H__
+#define __MUTEX_H__
+#include <stdint.h>
+#define LOCKED 1
+#define UNLOCKED 0
+
+typedef uint8_t mutex;
+
+int spin_lock_aquire(mutex *lock);
+void spin_lock_release(mutex *lock);
+#endif
---
+++ src/Kernel/Src/mutex.c
@@ -0,0 +1,24 @@
+#include <mutex.h>
+#include <stdint.h>
+#include <weeOs.h>
+int spin_lock_aquire(mutex *lock)
+{
+	uint8_t locked = 1;
+
+	do {
+		if (!__LDREXB(lock)) {
+			if(!__STREXB(1, lock)) {
+				__CLREX();
+				locked = 0;
+			}
+		}
+	} while(locked);
+
+	return 0;
+}
+
+void spin_lock_release(mutex *lock)
+{
+	__DMB();
+	*lock = 0;
+}
